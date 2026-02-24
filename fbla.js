66dec1e3e6f1ecc358cbf6a1fb69132b6c50b8cd
Sage Robinson <sager7513@gmail.com>
2026-02-23 20:24:19 -0500

    Dyslexia Mode: Commits

--- fbla.js
+++ fbla.js
@@ -1,5 +1,1 @@
 
-function setLanguage(lang) {
-  document.getElementById("title").textContent = translations[lang].title;
-  document.getElementById("desc").textContent = translations[lang].desc;
-}
